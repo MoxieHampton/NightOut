637172720240439108b9ff9e58836c42a3c4ca5d
Kurt Griarte <kurtgee11@gmail.com>
2017-07-12 08:20:02 -0400

    updated version 3

--- project1/game.js
+++ project1/game.js
@@ -2,5 +2,7 @@ $(document).ready(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
     $('.carousel').carousel();
+    $('.parallax').parallax();
+
 
 });
