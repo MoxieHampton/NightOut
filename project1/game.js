31c911792999147eb50eed5602430374592e4996
Kurt Griarte <kurtgee11@gmail.com>
2017-07-12 07:55:59 -0400

    updated version

--- project1/game.js
+++ project1/game.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-    $('.carousel.carousel-slider').carousel({
-        full_width: true
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+    $('.carousel').carousel();
 
-    });
 });
